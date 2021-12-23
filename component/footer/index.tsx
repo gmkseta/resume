b2aa49af8b43241e3fea9ca404fb7c9beca2002d
SeongjunKim <gmkseta@gmail.com>
2021-12-23 20:03:21 +0900

    add hit counter

--- component/footer/index.tsx
+++ component/footer/index.tsx
@@ -29,6 +29,14 @@ const Component = function ({ payload }: PropsWithChildren<{ payload: IFooter.Pa
               {payload.bootstrapVersion}
             </small>
           </EmptyRowCol>
+          <EmptyRowCol>
+            <a href="https://hits.seeyoufarm.com">
+              <img
+                src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fresume.seongjun.kr&count_bg=%23CF59D5&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"
+                alt="hit"
+              />
+            </a>
+          </EmptyRowCol>
           <br />
         </div>
       </Col>
